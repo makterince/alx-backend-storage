56d5022493ee183c1164eb42a6e244610b463f0a
Festus Omaku <fomaku3@gmail.com>
2023-08-12 13:54:34 +0100

    adv and mand

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+--  lists all bands with Glam rock as main style,
+-- ranked by longevity
+SELECT `band_name`,
+    COALESCE(`split`, 2022) - `formed` AS `lifespan`
+FROM `metal_bands`
+WHERE `style` LIKE '%Glam rock%'
+ORDER BY `lifespan` DESC;
---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,3 @@
+-- Creeates index on the names table using the
+-- first letter of name and score
+CREATE INDEX idx_name_first_score ON names(name(1), score);
